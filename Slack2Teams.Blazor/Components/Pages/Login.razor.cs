9e84415e87272281e3edc65d133b8b7e2c2580d1
Derek Cacciotti <applegamer247@gmail.com>
2024-08-05 10:46:13 -0400

    working login with validation

--- Slack2Teams.Blazor/Components/Pages/Login.razor.cs
+++ Slack2Teams.Blazor/Components/Pages/Login.razor.cs
@@ -12,20 +12,36 @@ public partial class Login : ComponentBase
     private  IAuthService _authService { get; set; }
     [Inject]
     private NavigationManager _navigationManager { get; set; }
-
     private LoginModel _model = new LoginModel();
     private RadzenLogin _loginForm;
-    private IEnumerable<string> errors;
+    private List<string> errors = new List<string>();
     private bool hasError;
+    private RadzenTemplateForm<LoginModel> _form { get; set; }
 
     private async Task LoginUser(LoginArgs obj)
     {
+        hasError = false;
+        errors.Clear();
         _model.UserName = obj.Username;
         _model.Password = obj.Password;
         var response = await _authService.Login(_model);
         if (response.IsSuccessful)
         {
             _navigationManager.NavigateTo("/");
+        }
+        else
+        {
+            if (response.Errors.Any())
+            {
+                foreach (var message in response.Errors)
+                {
+                    errors.Add(message);
+                }
+
+                hasError = true;
+            }
+
+           
         }
     }
 }
